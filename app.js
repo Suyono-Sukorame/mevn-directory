3d4ab1dce353f7f5d3bacaaaf6f257c72ff5ef34
Suyono-Sukorame <suyonosukorame@ymail.com>
2023-11-24 22:06:08 +0800

    delete

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const methodOverride = require("method-override");
 const path = require("path");
 const mongoose = require("mongoose");
 const Place = require("./models/place");
@@ -11,6 +12,7 @@ app.set("views", path.join(__dirname, "views"));
 
 // middleware
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 // Connect to MongoDB
 mongoose
@@ -41,9 +43,9 @@ app.post("/places", async (req, res) => {
   try {
     const place = new Place(req.body.place);
     await place.save();
-    res.redirect("/places"); // Redirect setelah menyimpan selesai
+    res.redirect("/places");
   } catch (err) {
-    console.error(err); // Tampilkan kesalahan pada konsol
+    console.error(err);
     res.status(500).send("Terjadi kesalahan saat menambahkan tempat baru: " + err.message);
   }
 });
@@ -54,6 +56,39 @@ app.get("/places/:id", async (req, res) => {
     res.render("places/show", { place });
   } catch (err) {
     res.status(500).send("Terjadi kesalahan saat memuat data place");
+  }
+});
+
+app.get("/places/:id/edit", async (req, res) => {
+  try {
+    const place = await Place.findById(req.params.id);
+    if (!place) {
+      return res.status(404).send("Tempat tidak ditemukan");
+    }
+    res.render("places/edit", { place });
+  } catch (err) {
+    res.status(500).send("Terjadi kesalahan saat memuat halaman edit");
+  }
+});
+
+app.put("/places/:id", async (req, res) => {
+  try {
+    const place = await Place.findByIdAndUpdate(req.params.id, { ...req.body.place });
+    if (!place) {
+      return res.status(404).send("Tempat tidak ditemukan");
+    }
+    res.redirect("/places");
+  } catch (err) {
+    res.status(500).send("Terjadi kesalahan saat memperbarui tempat");
+  }
+});
+
+app.delete("/places/:id", async (req, res) => {
+  try {
+    const place = await Place.findByIdAndDelete(req.params.id);
+    res.redirect("/places");
+  } catch (err) {
+    res.status(500).send("Terjadi kesalahan saat menghapus tempat");
   }
 });
 
