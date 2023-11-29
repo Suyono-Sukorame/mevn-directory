a8a2f46b9043480e8b5fb80c98d5b7b8eaee4a65
Suyono-Sukorame <suyonosukorame@ymail.com>
2023-11-29 18:00:43 +0800

    controller

--- routes/places.js
+++ routes/places.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const Place = require("../models/place");
+const PlaceController = require("../controllers/places");
 const { placeSchema } = require("../schemas/place");
 const ErrorHandler = require("../utils/ExpressError");
 const wrapAsync = require("../utils/wrapAsync");
 const isValidObjectId = require("../middlewares/isValidObjectId");
-const isAuth = require("../middlewares/isAuth.js");
-const { isAuthorPlace } = require("../middlewares/isAuthor.js");
+const isAuth = require("../middlewares/isAuth");
+const { isAuthorPlace } = require("../middlewares/isAuthor");
 
 const router = express.Router();
 
@@ -21,93 +21,18 @@ const validatePlace = (req, res, next) => {
 
 // Routes
 
-router.get(
-  "/",
-  isAuth,
-  wrapAsync(async (req, res) => {
-    const places = await Place.find();
-    res.render("places/index", { places });
-  })
-);
+router.get("/", wrapAsync(PlaceController.index));
 
 router.get("/create", isAuth, (req, res) => {
   res.render("places/create");
 });
 
-router.post(
-  "/",
-  isAuth,
-  validatePlace,
-  wrapAsync(async (req, res) => {
-    const place = new Place(req.body.place);
-    await place.save();
-    req.flash("success_msg", "Place added successfully");
-    res.redirect("/places");
-  })
-);
+router.post("/", isAuth, validatePlace, wrapAsync(PlaceController.store));
+// router.post("/", isAuth, validatePlace, wrapAsync(PlaceController.place));
+router.get("/:id", isValidObjectId("/places"), wrapAsync(PlaceController.show));
+router.get("/:id/edit", isAuth, isAuthorPlace, isValidObjectId("/places"), wrapAsync(PlaceController.edit));
+router.put("/:id", isAuth, isAuthorPlace, isValidObjectId("/places"), validatePlace, wrapAsync(PlaceController.update));
 
-router.get(
-  "/:id",
-  isValidObjectId("/places"),
-  wrapAsync(async (req, res) => {
-    const place = await Place.findById(req.params.id)
-      .populate({
-        path: "reviews",
-        populate: {
-          path: "author",
-        },
-      })
-      .populate("author");
-    // console.log(place); // nanti perlu dihapus
-    res.render("places/show", { place });
-  })
-);
-
-router.get(
-  "/:id/edit",
-  isAuth,
-  isAuthorPlace,
-  isValidObjectId("/places"),
-  wrapAsync(async (req, res) => {
-    const place = await Place.findById(req.params.id);
-    if (!place) {
-      return res.status(404).send("Tempat tidak ditemukan");
-    }
-    res.render("places/edit", { place });
-  })
-);
-
-router.put(
-  "/:id",
-  isAuth,
-  isAuthorPlace,
-  isValidObjectId("/places"),
-  validatePlace,
-  wrapAsync(async (req, res) => {
-    const { id } = req.params;
-    let place = await Place.findById(id);
-
-    if (!place.author.equals(req.user._id)) {
-      req.flash("error_msg", "Not authorized ");
-      return res.redirect("/places");
-    }
-
-    await Place.findByIdAndUpdate(req.params.id, { ...req.body.place }, { new: true });
-    req.flash("success_msg", "Place updated successfully");
-    res.redirect(`/places/${id}`);
-  })
-);
-
-router.delete(
-  "/:id",
-  isAuth,
-  isAuthorPlace,
-  isValidObjectId("/places"),
-  wrapAsync(async (req, res) => {
-    await Place.findByIdAndDelete(req.params.id);
-    req.flash("success_msg", "Place deleted successfully");
-    res.redirect("/places");
-  })
-);
+router.delete("/:id", isAuth, isAuthorPlace, isValidObjectId("/places"), wrapAsync(PlaceController.delete));
 
 module.exports = router;
