f8733ef559be3663e5f2d7d6819989de2ef30540
Suyono-Sukorame <suyonosukorame@ymail.com>
2023-12-01 07:53:46 +0800

    form-create

--- schemas/place.js
+++ schemas/place.js
@@ -1,11 +1,9 @@
 const Joi = require("joi");
 
 module.exports.placeSchema = Joi.object({
-  place: Joi.object({
-    title: Joi.string().required(),
-    description: Joi.string().required(),
-    location: Joi.string().required(),
-    price: Joi.number().min(0).required(),
-    image: Joi.string().allow(null, ""), // Mengizinkan field "image" kosong atau bernilai null
-  }).required(),
+  title: Joi.string().required(),
+  description: Joi.string().required(),
+  location: Joi.string().required(),
+  price: Joi.number().min(0).required(),
+  image: Joi.string().allow("").optional(), // Mengizinkan field "image" menjadi opsional atau boleh kosong
 });
