b337fa2afe6e92a903c85f2f4c2204afa3ee4188
Suyono-Sukorame <suyonosukorame@ymail.com>
2023-12-01 19:38:57 +0800

    carousel

--- schemas/place.js
+++ schemas/place.js
@@ -1,9 +1,11 @@
 const Joi = require("joi");
 
 module.exports.placeSchema = Joi.object({
-  title: Joi.string().required(),
-  description: Joi.string().required(),
-  location: Joi.string().required(),
-  price: Joi.number().min(0).required(),
-  image: Joi.string().allow("").optional(), // Mengizinkan field "image" menjadi opsional atau boleh kosong
+  place: Joi.object({
+    title: Joi.string().required(),
+    description: Joi.string().required(),
+    location: Joi.string().required(),
+    price: Joi.number().min(0).required(),
+    // image: Joi.string().required()
+  }).required(),
 });
